09970187fbe3af77486645ddbfc485a394ba27f4
Rachel <rzhangmoz1@gmail.com>
2018-06-24 19:07:49 -0700

    Update ajax.js

--- src/ajax.js
+++ src/ajax.js
@@ -1,8 +1,6 @@
 /* global fetch:false */
-
- const apiHost = "https://bakesaleforgood.com/api/deals"
-//const apiHost = "http://127.0.0.1:8080/v1/potteries.json"
-//const apiHost = "http://34.220.121.242:8080/v1/potteries.json"
+ 
+const apiHost = "http://127.0.0.1:8080/v1/potteries.json"
 
 export default {
     async fetchPotteryLists(){
